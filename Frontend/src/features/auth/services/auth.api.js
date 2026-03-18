2fb3d8374c14f8fdf77fb50f2e3bb0a1cba2804e
Sumit Mane <sumitmane8071@gmail.com>
2026-03-18 22:22:52 +0530

    fix: use env variables for API base URL

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4040";
+
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4040",
     withCredentials: true
 })
 
