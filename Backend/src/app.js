380e87baee31abb8d35cb45631790c6b989a71b8
Sumit Mane <sumitmane8071@gmail.com>
2026-03-22 18:12:38 +0530

    fix: update CORS config to allow Vercel origins dynamically

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,11 +7,25 @@ const CLIENT_URL = process.env.CLIENT_URL
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173",
+  "https://resume-fit-ai-beta.vercel.app",
+  "https://resume-fit-axs26r52y-sumit4261s-projects.vercel.app"
+].filter(Boolean);
+
 app.use(cors({
-    origin: CLIENT_URL,
-    credentials: true
-}
-))
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS blocked"));
+    }
+  },
+  credentials: true
+}));
 
 /* require all the routes here */ 
 const authRouter = require('./routes/auth.routes')
